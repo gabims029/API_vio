4143e2f2448f9773334d4474b84a19ce91984b04
gabims029 <gabimelo541@gmail.com>
2025-02-26 12:48:02 -0300

    Atualização API

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'10.89.240.69',
+    host:'localhost',
     user:'alunods',
     password:'senai@604',
     database:'vio_gabi'
